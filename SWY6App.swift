529913a0da693ef7e4af786019589e41bb50ed83
swaydating <lawrencewjones@icloud.com>
2025-10-21 12:14:46 +1100

    Create main app entry point SWY6App.swift

---
+++ SWY6App.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct SWY6App: App {
+    // Define any necessary state properties here
+
+    var body: some Scene {
+        WindowGroup {
+            // Replace with your initial view
+            ContentView()
+        }
+    }
+}
